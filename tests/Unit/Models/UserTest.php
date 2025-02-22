eae56cddf2d06ae73e40e85957892f7db52fe43d
Foulla <samankassoufoulla@gmail.com>
2025-02-22 22:11:49 +0100

    chore: remove laravel breeze files from test coverage

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+
+test('to array', function () {
+    $user = User::factory()->create()->fresh();
+
+    expect(array_keys($user->toArray()))
+        ->toEqual([
+            'id',
+            'name',
+            'email',
+            'email_verified_at',
+            'created_at',
+            'updated_at',
+        ]);
+});
