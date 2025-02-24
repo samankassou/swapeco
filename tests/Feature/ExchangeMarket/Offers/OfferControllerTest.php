a64aae79829227981ea824c1318d45a68906d47d
Foulla <samankassoufoulla@gmail.com>
2025-02-25 00:14:50 +0100

    wip

--- tests/Feature/ExchangeMarket/Offers/OfferControllerTest.php
+++ tests/Feature/ExchangeMarket/Offers/OfferControllerTest.php
@@ -13,7 +13,7 @@ it('can list offers', function () {
     $this->actingAs($user)
         ->get('/admin/exchange-market/offers')
         ->assertInertia(
-            fn(Assert $assert) => $assert
+            fn (Assert $assert) => $assert
                 ->component('ExchangeMarket/Offers/List/Index')
                 ->has('offers.data', 10)
         );
