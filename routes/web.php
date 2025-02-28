7b9ac50abdf78acf1a7f6a3a30a59d1d7707cc74
MICKY <mickynguelieuga@gmail.com>
2025-02-28 03:59:05 +0100

    Fix : Correct lint errors in web.php

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,5 @@ Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'admin', 'as' =>
     Route::post('/exchange-market/offers', [OfferController::class, 'store'])->name('exchange_market.offers.store');
 });
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
